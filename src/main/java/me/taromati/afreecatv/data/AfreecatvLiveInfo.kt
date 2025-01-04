66aec83217cf44c048bb3a2abf12e80e6e5e6b95
Choi-JinHwan <jinhwan0515@gmail.com>
2025-01-04 19:58:12 +0900

    fix: 컴파일 에러 해결

---
+++ src/main/java/me/taromati/afreecatv/data/AfreecatvLiveInfo.kt
@@ -0,0 +1,8 @@
+package me.taromati.afreecatv.data;
+
+data class AfreecatvLiveInfo(
+    val bjId: String,
+    val bjName: String,
+    val liveTitle: String,
+    val categoryTags: List<String>,
+)
