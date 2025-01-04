e194a74c731f5ad8ae9bd5a642999be41b759f4c
Choi-JinHwan <jinhwan0515@gmail.com>
2025-01-04 20:15:56 +0900

    feat: 타입 수정

--- src/main/java/me/taromati/afreecatv/event/implement/DonationChatEvent.kt
+++ src/main/java/me/taromati/afreecatv/event/implement/DonationChatEvent.kt
@@ -3,10 +3,10 @@ package me.taromati.afreecatv.event.implement
 import me.taromati.afreecatv.event.AfreecatvEvent
 
 data class DonationChatEvent(
-    val channelId: String? = null,
+    val channelId: String,
     val userId: String? = null,
-    val nickname: String? = null,
+    val nickname: String,
     val message: String? = null,
-    val payAmount: Int?,
-    val balloonAmount: Int?,
+    val payAmount: Int,
+    val balloonAmount: Int,
 ) : AfreecatvEvent
