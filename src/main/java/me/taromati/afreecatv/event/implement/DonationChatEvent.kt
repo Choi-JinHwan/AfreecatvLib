b03b33a3c78a849869248fba3570bd1f4ce07749
Choi-JinHwan <jinhwan0515@gmail.com>
2025-01-04 18:23:03 +0900

    feat: DonationChatEvent를 코틀린으로 변환

--- src/main/java/me/taromati/afreecatv/event/implement/DonationChatEvent.kt
+++ src/main/java/me/taromati/afreecatv/event/implement/DonationChatEvent.kt
@@ -1,19 +1,11 @@
-package me.taromati.afreecatv.event.implement;
+package me.taromati.afreecatv.event.implement
 
-import me.taromati.afreecatv.event.AfreecatvEvent;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import me.taromati.afreecatv.event.AfreecatvEvent
 
-@Getter
-@AllArgsConstructor
-public class DonationChatEvent implements AfreecatvEvent {
-
-    private final String channelId;
-
-    private final String nickname;
-    private final String message;
-
-    private final int payAmount;
-    private final int balloonAmount;
-
-}
+data class DonationChatEvent(
+    val channelId: String? = null,
+    val nickname: String? = null,
+    val message: String? = null,
+    val payAmount: Int?,
+    val balloonAmount: Int?,
+) : AfreecatvEvent
