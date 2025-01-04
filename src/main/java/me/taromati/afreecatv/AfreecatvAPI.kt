498438c47ee4b4b9025379985a6177a64314f978
Choi-JinHwan <jinhwan0515@gmail.com>
2025-01-04 18:37:26 +0900

    refactor: AfreecatvAPI 리팩터링

--- src/main/java/me/taromati/afreecatv/AfreecatvAPI.kt
+++ src/main/java/me/taromati/afreecatv/AfreecatvAPI.kt
@@ -1,186 +1,202 @@
-package me.taromati.afreecatv;
+package me.taromati.afreecatv
 
-import me.taromati.afreecatv.data.AfreecatvInfo;
-import me.taromati.afreecatv.data.AfreecatvLiveInfo;
-import me.taromati.afreecatv.exception.AfreecatvException;
-import me.taromati.afreecatv.exception.ExceptionCode;
-import me.taromati.afreecatv.listener.AfreecatvListener;
-import lombok.Getter;
-import org.java_websocket.drafts.Draft_6455;
-import org.java_websocket.protocols.Protocol;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-
-import java.net.URI;
-import java.net.URLEncoder;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.net.URI
+import java.net.URLEncoder
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpRequest.BodyPublisher
+import java.net.http.HttpResponse
+import java.nio.charset.StandardCharsets
+import lombok.Getter
+import me.taromati.afreecatv.data.AfreecatvInfo
+import me.taromati.afreecatv.data.AfreecatvLiveInfo
+import me.taromati.afreecatv.exception.AfreecatvException
+import me.taromati.afreecatv.exception.ExceptionCode
+import me.taromati.afreecatv.listener.AfreecatvListener
+import org.java_websocket.drafts.Draft_6455
+import org.java_websocket.protocols.IProtocol
+import org.java_websocket.protocols.Protocol
+import org.json.simple.JSONArray
+import org.json.simple.JSONObject
+import org.json.simple.parser.JSONParser
 
 @Getter
-public class AfreecatvAPI {
+class AfreecatvAPI(private var channelId: String?) {
+    private var socket: AfreecatvSocket? = null
 
-    private String channelId;
-    private AfreecatvSocket socket;
+    val listeners: MutableList<AfreecatvListener> = ArrayList()
 
-    public final List<AfreecatvListener> listeners = new ArrayList<>();
-
-    public AfreecatvAPI(final String channelId) {
-        this.channelId = channelId;
-    }
-
-    public AfreecatvAPI connect() {
-        if (!isConnected()) {
+    fun connect(): AfreecatvAPI {
+        if (!isConnected) {
             try {
-                AfreecatvInfo info = getInfo(this.channelId);
-                Draft_6455 draft6455 = new Draft_6455(
-                        Collections.emptyList(),
-                        Collections.singletonList(new Protocol("chat"))
-                );
-                AfreecatvSocket webSocket = new AfreecatvSocket(this, "wss://" + info.getChannelDomain() + ":" + info.getChannelPt() + "/Websocket/" + info.getStreamerId(), draft6455, info, this.channelId);
-                webSocket.connect();
-                this.socket = webSocket;
-                return this;
-            } catch (Exception e) {
-                this.channelId = null;
-                this.socket = null;
-                return this;
+                val info = getInfo(this.channelId)
+                val draft6455 = Draft_6455(
+                    emptyList(),
+                    listOf<IProtocol>(Protocol("chat"))
+                )
+                val webSocket = AfreecatvSocket(
+                    this,
+                    "wss://" + info.channelDomain + ":" + info.channelPt + "/Websocket/" + info.streamerId,
+                    draft6455,
+                    info,
+                    channelId!!
+                )
+                webSocket.connect()
+                this.socket = webSocket
+                return this
+            } catch (e: Exception) {
+                this.channelId = null
+                this.socket = null
+                return this
             }
         }
-        return this;
+        return this
     }
 
-    public AfreecatvAPI disconnect() {
+    fun disconnect(): AfreecatvAPI {
         if (this.socket != null) {
-            this.socket.close();
-            this.socket = null;
-            this.channelId = null;
+            socket!!.close()
+            this.socket = null
+            this.channelId = null
         }
-        return this;
+        return this
     }
 
-    public AfreecatvAPI addListeners(final AfreecatvListener... listeners) {
-        this.listeners.addAll(Arrays.asList(listeners));
-        return this;
+    fun addListeners(listeners: List<AfreecatvListener>): AfreecatvAPI {
+        this.listeners.addAll(listeners)
+        return this
     }
 
-    public boolean isConnected() {
-        return socket != null && !socket.isClosed();
+    val isConnected: Boolean
+        get() = socket != null && !socket!!.isClosed
+
+    class AfreecatvBuilder {
+        private var channelId: String? = null
+
+        fun withData(channelId: String?): AfreecatvBuilder {
+            this.channelId = channelId
+            return this
+        }
+
+        fun build(): AfreecatvAPI {
+            return createAPI(this.channelId)
+        }
     }
 
-    public static AfreecatvLiveInfo getLiveInfo(final String bjId) {
-        try {
-            HttpClient client = HttpClient.newHttpClient();
-            JSONObject bodyJson = new JSONObject();
-            bodyJson.put("bid", bjId);
-            bodyJson.put("type", "live");
-            bodyJson.put("confirm_adult", "false");
-            bodyJson.put("player_type", "html5");
-            bodyJson.put("mode", "landing");
-            bodyJson.put("from_api", "0");
-            bodyJson.put("pwd", "");
-            bodyJson.put("stream_type", "common");
-            bodyJson.put("quality", "HD");
-            HttpRequest request = HttpRequest.newBuilder().POST(formData(bodyJson))
-                    .uri(URI.create("https://live.afreecatv.com/afreeca/player_live_api.php?bjid=" + bjId))
-                    .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
-                    .header("Content-Type", "application/x-www-form-urlencoded").build();
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode() == 200) {
-                JSONParser parser = new JSONParser();
-                JSONObject jsonObject = (JSONObject) parser.parse(response.body());
-                JSONObject channel = (JSONObject) jsonObject.get("CHANNEL");
-                List<String> categoryTags = new ArrayList<>();
-                for (final Object s : (JSONArray) new JSONParser().parse(channel.get("CATEGORY_TAGS").toString())) {
-                    categoryTags.add(s.toString());
+    companion object {
+        fun getLiveInfo(bjId: String): AfreecatvLiveInfo {
+            try {
+                val response = HttpClient.newHttpClient().use { client ->
+                    val bodyJson = JSONObject(
+                        mapOf(
+                            "bid" to bjId,
+                            "type" to "live",
+                            "confirm_adult" to "false",
+                            "player_type" to "html5",
+                            "mode" to "landing",
+                            "from_api" to "0",
+                            "pwd" to "",
+                            "stream_type" to "common",
+                            "quality" to "HD",
+                        )
+                    )
+
+                    val request = HttpRequest.newBuilder().POST(formData(bodyJson))
+                        .uri(URI.create("https://live.afreecatv.com/afreeca/player_live_api.php?bjid=$bjId"))
+                        .header(
+                            "User-Agent",
+                            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+                        )
+                        .header("Content-Type", "application/x-www-form-urlencoded").build()
+                    client.send(request, HttpResponse.BodyHandlers.ofString())
                 }
-                return new AfreecatvLiveInfo(
-                        channel.get("BJID").toString(),
-                        channel.get("BJNICK").toString(),
-                        channel.get("TITLE").toString(),
-                        categoryTags
-                );
-            } else {
-                throw new AfreecatvException(ExceptionCode.API_CHAT_CHANNEL_ID_ERROR);
+                if (response.statusCode() != 200) {
+                    throw AfreecatvException(ExceptionCode.API_CHAT_CHANNEL_ID_ERROR)
+                }
+                val parser = JSONParser()
+                val jsonObject = parser.parse(response.body()) as JSONObject
+                val channel = jsonObject["CHANNEL"] as JSONObject
+                val categoryTags: MutableList<String> = ArrayList()
+                for (s in JSONParser().parse(channel["CATEGORY_TAGS"].toString()) as JSONArray) {
+                    categoryTags.add(s.toString())
+                }
+                return AfreecatvLiveInfo(
+                    channel["BJID"].toString(),
+                    channel["BJNICK"].toString(),
+                    channel["TITLE"].toString(),
+                    categoryTags
+                )
+            } catch (e: Exception) {
+                throw AfreecatvException(ExceptionCode.API_CHAT_CHANNEL_ID_ERROR)
             }
-        } catch (Exception e) {
-            throw new AfreecatvException(ExceptionCode.API_CHAT_CHANNEL_ID_ERROR);
-        }
-    }
-
-    public static AfreecatvAPI createAPI(final String channelId) {
-        return new AfreecatvAPI(channelId);
-    }
-
-    public static class AfreecatvBuilder {
-
-        private String channelId;
-
-        public AfreecatvBuilder withData(final String channelId) {
-            this.channelId = channelId;
-            return this;
         }
 
-        public AfreecatvAPI build() {
-            return AfreecatvAPI.createAPI(this.channelId);
+        fun createAPI(channelId: String?): AfreecatvAPI {
+            return AfreecatvAPI(channelId)
         }
 
-    }
-
-    private static AfreecatvInfo getInfo(String bjId) {
-        try {
-            HttpClient client = HttpClient.newHttpClient();
-            JSONObject bodyJson = new JSONObject();
-            bodyJson.put("bid", bjId);
-            bodyJson.put("type", "live");
-            bodyJson.put("confirm_adult", "false");
-            bodyJson.put("player_type", "html5");
-            bodyJson.put("mode", "landing");
-            bodyJson.put("from_api", "0");
-            bodyJson.put("pwd", "");
-            bodyJson.put("stream_type", "common");
-            bodyJson.put("quality", "HD");
-            HttpRequest request = HttpRequest.newBuilder().POST(formData(bodyJson))
-                    .uri(URI.create("https://live.afreecatv.com/afreeca/player_live_api.php?bjid=" + bjId))
-                    .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
-                    .header("Content-Type", "application/x-www-form-urlencoded").build();
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode() == 200) {
-                JSONParser parser = new JSONParser();
-                JSONObject jsonObject = (JSONObject) parser.parse(response.body());
-                JSONObject channel = (JSONObject) jsonObject.get("CHANNEL");
-                return new AfreecatvInfo(
-                        channel.get("CHDOMAIN").toString(),
-                        channel.get("CHATNO").toString(),
-                        String.valueOf(Integer.parseInt(channel.get("CHPT").toString()) + 1),
-                        channel.get("FTK").toString(),
-                        channel.get("TITLE").toString(),
-                        channel.get("BJID").toString(),
-                        channel.get("BNO").toString()
-                );
-            } else {
-                throw new AfreecatvException(ExceptionCode.API_CHAT_CHANNEL_ID_ERROR);
+        private fun getInfo(bjId: String?): AfreecatvInfo {
+            try {
+                val client = HttpClient.newHttpClient()
+                val bodyJson = JSONObject(
+                    mapOf(
+                        "bid" to bjId,
+                        "type" to "live",
+                        "confirm_adult" to "false",
+                        "player_type" to "html5",
+                        "mode" to "landing",
+                        "from_api" to "0",
+                        "pwd" to "",
+                        "stream_type" to "common",
+                        "quality" to "HD",
+                    )
+                )
+                val request = HttpRequest.newBuilder().POST(formData(bodyJson))
+                    .uri(URI.create("https://live.afreecatv.com/afreeca/player_live_api.php?bjid=$bjId"))
+                    .headers(
+                        "User-Agent",
+                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+                        "Content-Type", "application/x-www-form-urlencoded"
+                    ).build()
+                val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+                if (response.statusCode() == 200) {
+                    val parser = JSONParser()
+                    val jsonObject = parser.parse(response.body()) as JSONObject
+                    val channel = jsonObject["CHANNEL"] as JSONObject
+                    return AfreecatvInfo(
+                        channel["CHDOMAIN"].toString(),
+                        channel["CHATNO"].toString(),
+                        (channel["CHPT"].toString().toInt() + 1).toString(),
+                        channel["FTK"].toString(),
+                        channel["TITLE"].toString(),
+                        channel["BJID"].toString(),
+                        channel["BNO"].toString()
+                    )
+                } else {
+                    throw AfreecatvException(ExceptionCode.API_CHAT_CHANNEL_ID_ERROR)
+                }
+            } catch (e: Exception) {
+                throw AfreecatvException(ExceptionCode.API_CHAT_CHANNEL_ID_ERROR)
             }
-        } catch (Exception e) {
-            throw new AfreecatvException(ExceptionCode.API_CHAT_CHANNEL_ID_ERROR);
         }
-    }
 
-    private static HttpRequest.BodyPublisher formData(Map<Object, Object> data) {
-        var builder = new StringBuilder();
-        for (Map.Entry<Object, Object> entry : data.entrySet()) {
-            if (!builder.isEmpty()) {
-                builder.append("&");
+        private fun formData(data: JSONObject): BodyPublisher {
+            val builder = StringBuilder()
+            for ((key, value) in data) {
+                builder.apply {
+                    if (isNotEmpty()) {
+                        append("&")
+                    }
+                    append(urlEncode(key.toString()))
+                    append("=")
+                    append(urlEncode(value.toString()))
+                }
             }
-
-            builder.append(URLEncoder.encode(entry.getKey().toString(), StandardCharsets.UTF_8));
-            builder.append("=");
-            builder.append(URLEncoder.encode(entry.getValue().toString(), StandardCharsets.UTF_8));
+            return HttpRequest.BodyPublishers.ofString(builder.toString())
         }
-        return HttpRequest.BodyPublishers.ofString(builder.toString());
-    }
 
+        private fun urlEncode(data: String) {
+            URLEncoder.encode(data, StandardCharsets.UTF_8)
+        }
+    }
 }
